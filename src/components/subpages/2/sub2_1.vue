c6b5763d52740cf36268be0c7f4afa9505ad6727
wjdgus3608 <wjdgus3608@naver.com>
2023-06-12 13:08:39 +0900

    json fetch subpage로 리팩토링

--- src/components/subpages/2/sub2_1.vue
+++ src/components/subpages/2/sub2_1.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="sub2_1">
-       <div class="topContainer">
-            <CharNav navType="second_first"/>
-       </div>
-       <div class="bottomContainer">
-        bottom
-       </div>
+        <div class="topContainer">
+            <CharNav :fetchFiles="fetchFiles" :nowChar="nowChar" :nowIdx="nowIdx" @updateIdx="updateIdx" />
+        </div>
+        <div class="bottomContainer">
+            bottom
+            {{ this.nowChar }}
+            <img :src=imagePath alt="">
+        </div>
     </div>
 </template>
   
@@ -13,8 +15,43 @@
 import CharNav from '../CharNav.vue';
 export default {
     name: 'sub2_1',
-    components:{
+    components: {
         CharNav
+    },
+    created() {
+        this.getFilesFromJson()
+    },
+    data() {
+        return {
+            nowChar: '',
+            nowIdx: 0,
+            types: String('second_first').split('_'),
+            fetchFiles: [],
+            imagePath:'@assets/'
+        }
+    },
+    methods: {
+        updateIdx(c, idx) {
+            this.nowIdx = idx;
+            this.nowChar = c;
+            console.log(c);
+            this.imagePath = '../../../assets/'+this.types[0]+'/'+this.types[1]+'/'+c+'.png';
+            console.log(this.imagePath);
+            this.imagePath = encodeURIComponent(this.imagePath);
+        },
+        getFilesFromJson() {
+            fetch('/file.json')
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.fetchFiles = data[this.types[0] + ''][this.types[1] + ''];
+                    this.nowChar = this.fetchFiles[0];
+                    console.log("fetched" + this.fetchFiles);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     }
 }
 </script>
@@ -24,13 +61,15 @@ export default {
     width: 100%;
     height: 100%;
     background-color: yellow;
-    
+
 }
-.topContainer{
+
+.topContainer {
     padding: 10px;
     background-color: greenyellow;
 }
-.bottomContainer{
+
+.bottomContainer {
     background-color: pink;
 }
 </style>
--- src/components/subpages/CharNav.vue
+++ src/components/subpages/CharNav.vue
@@ -14,40 +14,21 @@
 
 export default {
     name: 'charNav',
-    props: ['navType'],
-    created() {
-        this.getFilesFromJson()
-    },
+    props: ['navType','nowChar','nowIdx','fetchFiles'],
     data() {
         return {
-            types: String(this.navType).split('_'),
-            fetchFiles: '',
-            nowChar: '',
-            nowIdx: 0
         }
     },
     methods: {
-        getFilesFromJson() {
-            fetch('/file.json')
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    this.fetchFiles = data[this.types[0] + ''][this.types[1] + ''];
-                    this.nowChar = this.fetchFiles[0]
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        },
         handleClick(idx,clickedFile) {
-            console.log(clickedFile);
+            console.log(this.nowChar);
             document.getElementById(this.nowChar).classList.remove('btn-primary');
             document.getElementById(this.nowChar).classList.remove('text-primary-emphasis');
 
-            this.nowChar = clickedFile;
-            this.nowIdx = idx;
-            document.getElementById(this.nowChar).className+=' btn-primary';
-            document.getElementById(this.nowChar).className+=' text-primary-emphasis';
+            this.$emit('updateIdx',clickedFile, idx);
+            document.getElementById(clickedFile).className+=' btn-primary';
+            document.getElementById(clickedFile).className+=' text-primary-emphasis';
+
 
         }
     },
